aee57980d113003ffb16959a9bbebbcbcc38670f
jmbarea <jmbarea@atsistemas.com>
2018-10-04 12:18:24 +0200

    Removes solution

--- src/main/java/com/at/test/curso/dao/BookDao.java
+++
@@ -1,9 +0,0 @@
-package com.at.test.curso.dao;
-
-import com.at.test.curso.model.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookDao extends JpaRepository<Book, Integer> {
-}
